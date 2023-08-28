09e67516332a4a32c47d02f280b3a6140e8fc49e
Michael Serhiienko <fleshrobot7@gmail.com>
2023-08-28 10:15:41 +0100

    Day1

---
+++ Tasks.py
@@ -0,0 +1,14 @@
+# Task 1
+list1 = [1, 2.2, 'abc', True, None]
+list1.pop(2)
+list1.reverse()
+
+list2 = [2, 3]
+# list1 = list1 + list2
+
+print(list1)
+
+# Task 2
+# list1 = list1.__add__(list2)
+list1.extend(list2)
+print(list1)

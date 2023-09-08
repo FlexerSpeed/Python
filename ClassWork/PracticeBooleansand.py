3da8728ad7364b56627585e78f80afbb1c2cf5b8
Michael Serhiienko <fleshrobot7@gmail.com>
2023-09-08 08:29:53 +0100

    (2)

--- ClassWork/PracticeBooleansand.py
+++ ClassWork/PracticeBooleansand.py
@@ -1,13 +1,8 @@
-def recomendation_to_travel(distance):
-    match distance:
-        case _ if distance < 3:
-            print('It\'s better to walk this distance')
-        case _ if 3 <= distance < 3000:
-            print('It\'s better to travel by car')
-        case _ if distance >= 3000:
-            print('It\'s better to fly by plane')
-
-
-your_way_distance = int(input('How far would you like to travel in miles? '))
-
-recomendation_to_travel(your_way_distance)
+distance = int(input('How far would you like to travel in miles? '))
+match distance:
+    case _ if distance < 3:
+        print('It\'s better to walk this distance')
+    case _ if 3 <= distance < 3000:
+        print('It\'s better to travel by car')
+    case _ if distance >= 3000:
+        print('It\'s better to fly by plane')
--- dfsf.py
+++ dfsf.py
@@ -13,3 +13,14 @@ for i, letter in enumerate('abcde', start=2):
 
 l = list(enumerate('abcde'))
 print(l)
+
+total = []
+for number in range(2, 101, 2):
+    total.append(number)
+print(total)
+
+for even in range(2, 11, 2):
+    print(even)
+    for odd in range(1, 11, 2):
+        val = even + odd
+        print(f'{even} + {odd} = {val}')
